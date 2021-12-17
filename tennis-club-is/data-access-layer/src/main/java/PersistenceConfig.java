6e8be95e72dba6d229bbf01c38013674231c0179
Monika Šlachtová <469194@mail.muni.cz>
2021-12-17 11:38:56 +0100

    Add javadoc to PersistenceConfig.java

--- tennis-club-is/data-access-layer/src/main/java/PersistenceConfig.java
+++ tennis-club-is/data-access-layer/src/main/java/PersistenceConfig.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
+ * Persistence configuration
  * @author Monika Slachtova
  */
 @Configuration
