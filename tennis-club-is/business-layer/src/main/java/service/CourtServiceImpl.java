3599a2756f675f7df54285f93bef733c74a54c7c
Monika Šlachtová <469194@mail.muni.cz>
2021-12-20 14:52:57 +0100

    Implement CourtService

---
+++ tennis-club-is/business-layer/src/main/java/service/CourtServiceImpl.java
@@ -0,0 +1,37 @@
+package service;
+
+import io.mslachtova.dao.CourtDao;
+import io.mslachtova.entity.Court;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Monika Slachtova
+ */
+@Service
+public class CourtServiceImpl implements CourtService {
+    @Autowired
+    private CourtDao courtDao;
+
+    @Override
+    public void create(Court court) {
+        courtDao.create(court);
+    }
+
+    @Override
+    public Court findById(Long id) {
+        return courtDao.findById(id);
+    }
+
+    @Override
+    public List<Court> findAll() {
+        return courtDao.findAll();
+    }
+
+    @Override
+    public Court findByCourtNumber(int courtNumber) {
+        return courtDao.findByCourtNumber(courtNumber);
+    }
+}
