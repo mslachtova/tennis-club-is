c6ce97a74918675245cc68e600adf9dc2d78640b
Monika Šlachtová <469194@mail.muni.cz>
2021-12-20 09:22:33 +0100

    Define User entity class

---
+++ tennis-club-is/data-access-layer/src/main/java/entity/User.java
@@ -0,0 +1,72 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Class encapsulating basic user information
+ *
+ * @author Monika Slachtova
+ */
+@Entity
+@Table(name="\"User\"")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true)
+    @NotNull
+    private String telephoneNumber;
+
+    @NotNull
+    private String name;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getTelephoneNumber().equals(user.getTelephoneNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTelephoneNumber());
+    }
+}
