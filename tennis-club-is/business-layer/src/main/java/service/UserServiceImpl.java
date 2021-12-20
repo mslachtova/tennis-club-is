007f15711bbdde88cc903783bdf060db783acf32
Monika Šlachtová <469194@mail.muni.cz>
2021-12-20 15:00:33 +0100

    Implement UserService

---
+++ tennis-club-is/business-layer/src/main/java/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package service;
+
+import io.mslachtova.dao.UserDao;
+import io.mslachtova.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Monika Slachtova
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void create(User user) {
+        userDao.create(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userDao.findById(id);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userDao.findAll();
+    }
+
+    @Override
+    public User findByTelephoneNumber(int telephoneNumber) {
+        return userDao.findByTelephoneNumber(telephoneNumber);
+    }
+}
