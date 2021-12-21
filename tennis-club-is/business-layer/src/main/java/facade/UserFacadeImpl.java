01fbd3fd39e4c728c33f7b596461a20d12b78332
Monika Šlachtová <469194@mail.muni.cz>
2021-12-21 11:56:11 +0100

    Implement UserFacade

---
+++ tennis-club-is/business-layer/src/main/java/facade/UserFacadeImpl.java
@@ -0,0 +1,44 @@
+package facade;
+
+import dto.UserDto;
+import io.mslachtova.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import service.BeanMapper;
+import service.UserService;
+
+import java.util.List;
+
+/**
+ * @author Monika Slachtova
+ */
+@Service
+@Transactional
+public class UserFacadeImpl implements UserFacade {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BeanMapper beanMapper;
+
+    @Override
+    public void create(UserDto user) {
+        userService.create(beanMapper.mapTo(user, User.class));
+    }
+
+    @Override
+    public UserDto findById(Long id) {
+        return beanMapper.mapTo(userService.findById(id), UserDto.class);
+    }
+
+    @Override
+    public List<UserDto> findAll() {
+        return beanMapper.mapTo(userService.findAll(), UserDto.class);
+    }
+
+    @Override
+    public UserDto findByTelephoneNumber(int telephoneNumber) {
+        return null;
+    }
+}
