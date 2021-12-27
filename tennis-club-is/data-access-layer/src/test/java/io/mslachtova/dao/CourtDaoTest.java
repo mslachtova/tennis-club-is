a6ed4fd7db6f592cb53ae21976a38f8f0c0d8d09
Monika Šlachtová <469194@mail.muni.cz>
2021-12-27 14:14:52 +0100

    Implement tests for CourtDaoTest

---
+++ tennis-club-is/data-access-layer/src/test/java/io/mslachtova/dao/CourtDaoTest.java
@@ -0,0 +1,111 @@
+package io.mslachtova.dao;
+
+import io.mslachtova.PersistenceConfig;
+import io.mslachtova.entity.Court;
+import io.mslachtova.entity.CourtSurface;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+/**
+ * @author Monika Slachtova
+ */
+@ContextConfiguration(classes = PersistenceConfig.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+class CourtDaoTest {
+    @PersistenceUnit
+    private EntityManagerFactory emf;
+
+    @Autowired
+    private CourtDao courtDao;
+
+    private CourtSurface courtSurface1;
+    private CourtSurface courtSurface2;
+
+    private Court court1;
+    private Court court2;
+
+    @BeforeEach
+    void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            courtSurface1 = new CourtSurface();
+            courtSurface1.setSurfaceType("grass");
+            courtSurface1.setPrice(200.0);
+            em.persist(courtSurface1);
+
+            courtSurface2 = new CourtSurface();
+            courtSurface2.setSurfaceType("hard");
+            courtSurface2.setPrice(180.0);
+            em.persist(courtSurface2);
+
+            court1 = new Court();
+            court1.setCourtSurface(courtSurface1);
+            em.persist(court1);
+
+            court2 = new Court();
+            court2.setCourtSurface(courtSurface2);
+            em.persist(court2);
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    void create() {
+        Court court = new Court();
+        court.setCourtSurface(courtSurface2);
+        courtDao.create(court);
+        assertThat(courtDao.findById(court.getId())).isEqualTo(court);
+    }
+
+    @Test
+    void createNullCourtSurface() {
+        assertThrows(DataAccessException.class, () -> {
+           Court court = new Court();
+           courtDao.create(court);
+        });
+    }
+
+    @Test
+    void findById() {
+        assertThat(courtDao.findById(court1.getId())).isEqualTo(court1);
+    }
+
+    @Test
+    void findAll() {
+        List<Court> foundCourts = courtDao.findAll();
+        assertThat(foundCourts.size()).isEqualTo(2);
+        assertThat(foundCourts).contains(court1, court2);
+    }
+
+    @Test
+    void findByCourtNumber() {
+        assertThat(courtDao.findByCourtNumber(court2.getCourtNumber())).isEqualTo(court2);
+    }
+
+    @Test
+    void findByNonExistingCourtNumber() {
+        assertThat(courtDao.findByCourtNumber(6)).isNull();
+    }
+}
