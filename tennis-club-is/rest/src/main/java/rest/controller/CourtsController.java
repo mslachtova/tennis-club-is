807d37257efd40f580b7f3d0f82cc5021aab6f18
Monika Šlachtová <469194@mail.muni.cz>
2021-12-27 09:26:57 +0100

    Implement CourtsController

---
+++ tennis-club-is/rest/src/main/java/rest/controller/CourtsController.java
@@ -0,0 +1,38 @@
+package rest.controller;
+
+import dto.CourtDto;
+import facade.CourtFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import rest.ApiUris;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * @author Monika Slachtova
+ */
+@RestController
+@RequestMapping(ApiUris.ROOT_URI_COURTS)
+public class CourtsController {
+    final static Logger logger = LoggerFactory.getLogger(CourtsController.class);
+
+    @Inject
+    private CourtFacade courtFacade;
+
+    /**
+     * Get list of courts curl -i -X GET
+     * http://localhost:8080/rest/wines
+     *
+     * @return CourtDto
+     */
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final List<CourtDto> getCourts() {
+        logger.info("rest getCourts()");
+        return courtFacade.findAll();
+    }
+}
