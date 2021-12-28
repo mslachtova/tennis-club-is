95e71aefe11429e5eb64ce4460a43d0f4151d2fd
Monika Šlachtová <469194@mail.muni.cz>
2021-12-28 09:45:50 +0100

    Add context configuration to CourtServiceTest

--- tennis-club-is/business-layer/src/test/java/io/mslachtova/service/CourtSurfaceServiceTest.java
+++ tennis-club-is/business-layer/src/test/java/io/mslachtova/service/CourtSurfaceServiceTest.java
@@ -1,13 +1,19 @@
 package io.mslachtova.service;
 
 
+import io.mslachtova.ServiceConfig;
 import io.mslachtova.dao.CourtSurfaceDao;
 import io.mslachtova.entity.CourtSurface;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
@@ -16,6 +22,10 @@ import static org.mockito.Mockito.when;
 /**
  * @author Monika Slachtova
  */
+@ContextConfiguration(classes = ServiceConfig.class)
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@Transactional
 class CourtSurfaceServiceTest {
     @Mock
     private CourtSurfaceDao courtSurfaceDao;
