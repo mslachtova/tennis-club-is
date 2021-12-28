4113f8e8fd2085f12d63070e3b89cd25b96b8b15
Monika Šlachtová <469194@mail.muni.cz>
2021-12-28 09:00:12 +0100

    Optimize imports and simplify methods

--- tennis-club-is/data-access-layer/src/test/java/io/mslachtova/dao/TestHelper.java
+++ tennis-club-is/data-access-layer/src/test/java/io/mslachtova/dao/TestHelper.java
@@ -1,12 +1,7 @@
 package io.mslachtova.dao;
 
-import io.mslachtova.entity.Court;
 import io.mslachtova.entity.CourtSurface;
-import io.mslachtova.entity.Reservation;
 import io.mslachtova.entity.User;
-import io.mslachtova.enums.GameType;
-
-import java.time.LocalDateTime;
 
 /**
  * Class including methods used by Test classes
@@ -15,22 +10,18 @@ import java.time.LocalDateTime;
  */
 public class TestHelper {
     static CourtSurface getGrassCourtSurface() {
-        CourtSurface courtSurface = new CourtSurface("grass", 200.0);
-        return courtSurface;
+        return new CourtSurface("grass", 200.0);
     }
 
     static CourtSurface getHardCourtSurface() {
-        CourtSurface courtSurface = new CourtSurface("hard", 180.0);
-        return courtSurface;
+        return new CourtSurface("hard", 180.0);
     }
 
     static User getAlice() {
-        User user = new User("745558238", "Alice Smith");
-        return user;
+        return new User("745558238", "Alice Smith");
     }
 
     static User getBob() {
-        User user = new User("766456991", "Bob Taylor");
-        return user;
+       return new User("766456991", "Bob Taylor");
     }
 }
